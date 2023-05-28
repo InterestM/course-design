79343b2d289af9705563fbf863d2498d9a7a6501
fiveeyesallblind <interestm@outlook.com>
2023-05-29 02:12:38 +0800

    Improved tab rendering

--- src/tui/tabs.cpp
+++ src/tui/tabs.cpp
@@ -1,16 +1,34 @@
+#include <chrono> // for operator""ms, literals
 #include <vector> // for vector
 
-#include "ftxui/component/component.hpp" // for Checkbox, Renderer, Horizontal, Vertical, Input, Menu, Radiobox, ResizableSplitLeft, Tab
+#include "ftxui/component/animation.hpp"         // for BackOut, Duration
+#include "ftxui/component/component.hpp"         // for Menu, Renderer, Vertical
+#include "ftxui/component/component_options.hpp" // for MenuOption, UnderlineOption
+#include "ftxui/screen/color.hpp" // for Color, Color::Blue, Color::Red
 
 #include "mainTab.cpp"
 using namespace ftxui;
-int tab_index = -1;
+int tab_index = 0;
 
 std::vector<std::string> tab_entries = {
     "Main", "empty", "empty", "empty", "empty", "Help",
 };
-auto tab_selection =
-    Menu(&tab_entries, &tab_index, MenuOption::HorizontalAnimated());
+
+// should be customizable
+class optional {
+public:
+  MenuOption option = MenuOption::HorizontalAnimated();
+  optional() {
+    using namespace std::literals; // for operator""ms
+    option.underline.SetAnimationFunction(animation::easing::BackOut);
+    option.underline.color_inactive = Color::Blue;
+    // option.underline.color_active = Color::Red;
+    option.underline.SetAnimationDuration(350ms);
+    // option.underline.follower_delay = 250ms;
+  }
+} tabOption;
+
+auto tab_selection = Menu(&tab_entries, &tab_index, tabOption.option);
 auto tab_content = Container::Tab(
     {
         mainTab,
